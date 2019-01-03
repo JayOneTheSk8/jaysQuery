f772bf9c0058c69bccf7c134a850bf732e3a2fa3
Justin <misterjustincox@gmail.com>
2019-01-03 13:35:00 -0500

    Complete about section

--- src/docs.js
+++ src/docs.js
@@ -2,8 +2,19 @@ const changeDefinition = (html) => {
   $definition.html(html);
 };
 
+const linkTo = (section) => {
+  return e => {
+    changeDefinition(section);
+  };
+};
+
+const setClick = (queryObject, section) => {
+  queryObject.on('click', linkTo(section));
+};
+
 document.addEventListener("DOMContentLoaded", (e) => {
   about = $j('#about').html();
+  setClick($j('#to-about'), about);
   $definition = $j('.definition');
   $sections = $j('.definition-section');
   $sections.remove();
